723cff46ea607ce83ed1180782c7c27c32b55ab2
mariaMeNow <mariiamenow@gmail.com>
2024-11-21 12:08:51 +0200

    add functionality to view menu button

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Home.css';
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx';
@@ -6,17 +6,28 @@ import FoodDisplay from '../../components/FoodDisplay/FoodDisplay.jsx';
 import AppDownload from '../../components/AppDownload/AppDownload.jsx';
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+  
+  // Create refs for ExploreMenu and FoodDisplay sections
+  const exploreMenuRef = useRef(null);
 
-const [category, setCategory] = useState("All");
+  const handleScrollToMenu = () => {
+    // Scroll to ExploreMenu section
+    if (exploreMenuRef.current) {
+      exploreMenuRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <div>
-      <Header/>
-      <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay category={category} setCategory={setCategory}/>
-      <AppDownload/>
+      <Header onViewMenuClick={handleScrollToMenu} />
+      <div ref={exploreMenuRef}>
+        <ExploreMenu category={category} setCategory={setCategory} />
+        <FoodDisplay category={category} setCategory={setCategory} />
+      </div>
+      <AppDownload />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
