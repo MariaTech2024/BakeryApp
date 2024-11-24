caf9288e71fee238ef8b2ba832df65e1ca75551b
mariaMeNow <mariiamenow@gmail.com>
2024-11-24 09:09:38 +0200

    add changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,6 @@ app.get("/", (req,res)=>{
 });
 
 app.listen(port, ()=>{
-    console.log(`Server is running on http://localhost:${port}`)
+    console.log(`Server is running on https://localhost:${port}`)
 })
 
