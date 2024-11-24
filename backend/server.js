b028ee17ef7d73f51a57190b4490335fb8e642ab
mariaMeNow <mariiamenow@gmail.com>
2024-11-24 12:22:18 +0200

    add vercel config

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,33 @@
 import express from "express";
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 
 dotenv.config();
-
 const app = express();
-const port = 4000;
-
-/// Get the current directory name
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
-
-
-
-app.get("/api/hello", (req, res) => {
-    res.send({ message: "Hello from the backend!" });
-  });
-
-//middleware
-app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",  
-  methods: 'GET, POST',            
-  credentials: true                 
-}));
 
 //db connection
 connectDB();
+
+//middleware
+app.use(cors());
+app.use(cors({
+   origin: 'http://localhost:3000'  
+ }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server deployed and running on vercel.");
+});
 
 //API endpoint
 
@@ -44,12 +37,12 @@ app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
-
+const port = process.env.PORT || 4000;
 
 app.get("/", (req,res)=>{
     res.send("Server is working!")
 });
 
-export default (req, res) => {
-  app(req, res);  // Vercel serverless handler
-};
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
